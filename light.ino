ce332965f87feceac08f34e819283ea61ebe526f
Little <admin@sixming.com>
2019-11-26 23:24:16 +0800

    细节数据修改

--- light.ino
+++ light.ino
@@ -3,7 +3,7 @@
 #include <ESP8266WebServer.h>
 #define pin 2
 int angle = 90;
-int speed = 35;
+int speed = 90;
 int delay_time = 200;
 int back_delay_time = 200;
 char ssid[] = "Little";
@@ -32,8 +32,8 @@ void handleNotFound();
 void Led();
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  
-  digitalWrite(LED_BUILTIN, LOW);  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   init_basic();
   init_wifi();
   init_servo();
@@ -65,7 +65,7 @@ void init_basic() {
 }
 void init_wifi() {
   WiFi.mode(WIFI_STA);
-  WiFi.config(ip,gateway,subnet);
+  WiFi.config(ip, gateway, subnet);
   WiFi.setAutoConnect(true);
   WiFi.setAutoReconnect(true);
   while (WiFi.status() != WL_CONNECTED)
@@ -90,7 +90,7 @@ void init_server() {
 
 
 void handleRoot() {
-  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", "The service of web is ok");
   Led();
 }
@@ -99,7 +99,7 @@ void handleOpen() {
 
 
   open();
-  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", "Light is open");
   Led();
 
@@ -107,40 +107,45 @@ void handleOpen() {
 void handleClose() {
   //Serial.println("close");
   close();
-  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", "Light is close");
   Led();
 }
 void handleSetting() {
   String message;
-
+  String a;
   for (int i = 0; i < server.args(); i++) {
     message += server.argName(i) + " : " +  server.arg(i) + "\n";
     if (server.argName(i).equals("speed")) {
       speed = server.arg(i).toInt();
+      a += "\n speed = " + server.arg(i).toInt();
     }
     else if (server.argName(i).equals("delay")) {
       delay_time = server.arg(i).toInt();
+      a += "\n delay= " + server.arg(i).toInt();
     }
-    else if(server.argName(i).equals("delay_back")){
+    else if (server.argName(i).equals("delay_back")) {
       back_delay_time = server.arg(i).toInt();
-      }
+      a += "\n delay_delay= " + server.arg(i).toInt();
+    }
     else {}
   }
-
-  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", message);
-    Led();
   Led();
+
+
+
+
 }
 void handleNotFound() {
-  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", "404");
 }
 
-void Led(){
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-    digitalWrite(LED_BUILTIN, HIGH);
-  
-  }
+void Led() {
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+}
